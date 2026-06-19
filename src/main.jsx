067fd7d9f6bd8b9602af5dba4eb07683fb620751
Lucas Brandão Cabral <lucasbrandao11br@gmail.com>
2026-06-19 14:41:17 +0000

    Revert "Usa a fonte Montserrat (embutida) na figurinha, mais proxima do original"
    This reverts commit 6ce5ba08f2bde0d3dfe597f3ed9e16c14b0ba126.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '@fontsource/montserrat/600.css'
-import '@fontsource/montserrat/700.css'
-import '@fontsource/montserrat/800.css'
-import '@fontsource/montserrat/900.css'
 import App from './App.jsx'
 import './index.css'
 
